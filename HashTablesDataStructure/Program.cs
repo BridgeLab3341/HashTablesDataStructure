e3dfc15286b34babfad047e54fcf0cc8f7cfbe80
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-19 13:51:20 +0530

    [somu]Add:Finding Frequency of Large Paragraph.

--- HashTablesDataStructure/Program.cs
+++ HashTablesDataStructure/Program.cs
@@ -4,8 +4,34 @@
     {
         public static void Main(string[] args)
         {
-            HashTables tables = new HashTables();
-            tables.FindFrequency();
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            HashTable<string,int> wordFrequency = GetWordFrequency(paragraph);
+
+            Console.WriteLine("Word Frequency:");
+            foreach (var entry in wordFrequency)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+        }
+        static HashTable<string, int> GetWordFrequency(string paragraph)
+        {
+            HashTable<string, int> wordFrequency = new HashTable<string, int>();
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                string cleanedWord = word.Trim().ToLower();
+
+                if (!string.IsNullOrEmpty(cleanedWord))
+                {
+                    int frequency = wordFrequency.Get(cleanedWord);
+                    wordFrequency.Add(cleanedWord, frequency + 1);
+                }
+            }
+
+            return wordFrequency;
         }
     }
 }
